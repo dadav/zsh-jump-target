99f3646ffe5ac962cf5dd653ce67b47f40bc31e1
dadav <33197631+dadav@users.noreply.github.com>
2024-03-17 14:24:06 +0100

    fix: Adjust prefixes

--- jump-target.plugin.zsh
+++ jump-target.plugin.zsh
@@ -1,57 +1,57 @@
 # Jump to a character in the command line
 function jump-target() {
-    typeset -g ZSH_JUMP_TARGET_CHOICES
-    typeset -g ZSH_JUMP_TARGET_STYLE
+  typeset -g ZSH_JUMP_TARGET_CHOICES
+  typeset -g ZSH_JUMP_TARGET_STYLE
 
-    : ${ZSH_JUMP_TARGET_CHOICES:="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-    : ${ZSH_JUMP_TARGET_STYLE:="fg=red,bold,underline"}
+  : ${ZSH_JUMP_TARGET_CHOICES:="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+  : ${ZSH_JUMP_TARGET_STYLE:="fg=red,bold,underline"}
 
-    local orig_buffer=$BUFFER
-    local -a orig_region_highlight
-    orig_region_highlight=("${region_highlight[@]}")
+  local orig_buffer=$BUFFER
+  local -a orig_region_highlight
+  orig_region_highlight=("${region_highlight[@]}")
 
-    {
-        zle -R "Char:"
-        local char
-        read -k char
+  {
+      zle -R "Char:"
+      local char
+      read -k char
 
-        integer buffer_idx
-        local target_choices_len=$#ZSH_JUMP_TARGET_CHOICES
-        local target_idx=1
-        local target_choice
-        local -A target_to_buffer_idx
-        local start
-        region_highlight=()
-        while buffer_idx=${BUFFER[(ib:buffer_idx+1:)$char]}
-              (( buffer_idx <= $#BUFFER ))
-        do
-            target_choice=${ZSH_JUMP_TARGET_CHOICES[target_idx]}
-            BUFFER[buffer_idx]=$target_choice
-            target_to_buffer_idx[$target_choice]=$buffer_idx
-            (( start = buffer_idx - 1 ))
-            region_highlight+=( "$start $buffer_idx $ZSH_JUMP_TARGET_STYLE" )
-            (( target_idx++ ))
-            if (( target_idx > target_choices_len )); then
-                break
-            fi
-        done
+      integer buffer_idx
+      local target_choices_len=$#ZSH_JUMP_TARGET_CHOICES
+      local target_idx=1
+      local target_choice
+      local -A target_to_buffer_idx
+      local start
+      region_highlight=()
+      while buffer_idx=${BUFFER[(ib:buffer_idx+1:)$char]}
+            (( buffer_idx <= $#BUFFER ))
+      do
+          target_choice=${ZSH_JUMP_TARGET_CHOICES[target_idx]}
+          BUFFER[buffer_idx]=$target_choice
+          target_to_buffer_idx[$target_choice]=$buffer_idx
+          (( start = buffer_idx - 1 ))
+          region_highlight+=( "$start $buffer_idx $ZSH_JUMP_TARGET_STYLE" )
+          (( target_idx++ ))
+          if (( target_idx > target_choices_len )); then
+              break
+          fi
+      done
 
-        if (( target_idx == 1 )); then
-            return 0
-        fi
+      if (( target_idx == 1 )); then
+          return 0
+      fi
 
-        zle -R "Target:"
-        read -k target_choice
+      zle -R "Target:"
+      read -k target_choice
 
-        if (( ${+target_to_buffer_idx[$target_choice]} )); then
-            (( CURSOR = target_to_buffer_idx[$target_choice] - 1 ))
-        fi
+      if (( ${+target_to_buffer_idx[$target_choice]} )); then
+          (( CURSOR = target_to_buffer_idx[$target_choice] - 1 ))
+      fi
 
-    } always {
-        BUFFER=$orig_buffer
-        region_highlight=("${orig_region_highlight[@]}")
-        zle -cR ''
-    }
+  } always {
+      BUFFER=$orig_buffer
+      region_highlight=("${orig_region_highlight[@]}")
+      zle -cR ''
+  }
 }
 zle -N jump-target
 bindkey "\ej" jump-target
